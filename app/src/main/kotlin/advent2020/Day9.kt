919a8e07f4cb426a9923fa2de6a4e56e85684c0b
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2020-12-09 20:13:59 +0100

    part 2.

--- app/src/main/kotlin/advent2020/Day9.kt
+++ app/src/main/kotlin/advent2020/Day9.kt
@@ -3,16 +3,19 @@ package advent2020
 fun main() {
     val inputs = fileToArr("app/src/main/resources/day9_1.txt")
 
-    part1(inputs)
+    val res = part1(inputs)
+    println(res)
+
+    part2(inputs, res)
 }
 
-private fun hasSum(sum: Long, lst: List<Long>): Boolean {
-    return lst.find { n ->
-        lst.find { it != n && it + n == sum } != null
-    } != null
-}
+private fun part1(inputs: List<String>): Long {
+    fun hasSum(sum: Long, lst: List<Long>): Boolean {
+        return lst.find { n ->
+            lst.find { it != n && it + n == sum } != null
+        } != null
+    }
 
-private fun part1(inputs: List<String>) {
     val preamble = 25
 
     val nums = inputs.map { it.toLong() }
@@ -23,7 +26,35 @@ private fun part1(inputs: List<String>) {
         !hasSum(num.value, last)
     }
 
-    println(result)
+    return result.value
+}
+
+private fun part2(inputs: List<String>, target: Long) {
+    val nums = inputs.map { it.toLong() }
+
+    fun findNums(start: Int, end: Int): String {
+        val sum = nums.slice(start..end).sum()
+        if (sum > target) return "over"
+        if (sum == target) return "match"
+        return "continue"
+    }
+
+    var st = 0
+    var endd = st + 1
+    var res = findNums(st, endd)
+    while (res != "match") {
+        if (res == "continue") {
+            endd += 1
+        }
+        if (res == "over") {
+            st += 1
+            endd = st + 1
+        }
+        res = findNums(st, endd)
+    }
+
+    val arr = nums.slice(st..endd)
+    println(arr.min()!! + arr.max()!!)
 }
 
 
